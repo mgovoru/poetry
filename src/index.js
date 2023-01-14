31ef33b7f7662e1cdc00c4b9d69b41e3bf91ef7e
mgovoru <m.govoru@yandex.ru>
2023-01-14 14:13:13 +0500

    add: files project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './index.html';
+import './styles/style.sass';
+
+let zSpacing = -1000,
+	lastpos = zSpacing / 5,
+	arrayValues = [];
+
+let frameArray = Array.from(document.getElementsByClassName('frame'));
+console.log(frameArray);
+window.onscroll = function () {
+	let top = document.documentElement.scrollTop,
+		delta = lastpos - top;
+	lastpos = top;
+	frameArray.forEach((n, i) => {
+		arrayValues.push((i * zSpacing) + zSpacing);
+		arrayValues[i] += delta * - 5.5;
+		console.log(arrayValues[i]);
+		let frame = frameArray[i],
+			transform = `translateZ(${arrayValues[i]}px)`,
+			opacity = arrayValues[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`);
+	})
+}
+window.scrollTo(0, 1);
